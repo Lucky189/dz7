14515b69f011b6a62a283fc6ddeabc1d917880c4
Бордюжа Марк <borduzamark@gmail.com>
2025-02-05 22:23:09 +0200

    Add files via upload

---
+++ dz7.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+void Function() 
+{
+    static int a = 0;
+    a++;
+    std::cout << a << std::endl;
+
+}
+
+int main() 
+{
+    for (int i = 0; i < 3; i++) 
+    {
+        Function();
+    }
+
+    return 0;
+}
